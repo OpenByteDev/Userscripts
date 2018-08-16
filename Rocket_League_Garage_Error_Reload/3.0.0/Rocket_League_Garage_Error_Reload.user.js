32e48e339fbd799c2fe3708dca6c150d46d53d32
OpenByte <development.openbyte@gmail.com>
2018-08-17 00:55:31 +0200

    Add new versions

---
+++ Rocket_League_Garage_Error_Reload/3.0.0/Rocket_League_Garage_Error_Reload.user.js
@@ -0,0 +1,20 @@
+﻿// ==UserScript==
+// @name            Rocket League Garage Error Reload
+// @namespace       openbyte/aerl
+// @author          OpenByte
+// @description     Refreshes the page when an Error occurs.
+// @icon            https://image.ibb.co/g9caQm/rocket_league_garage_footer.png
+// @include         http*://rocket-league.com/*
+// @include         http*://*.rocket-league.com/*
+// @license         MIT License
+// @encoding        utf-8
+// @compatible      firefox
+// @compatible      chrome
+// @compatible      opera
+// @version         3.0.0
+// @grant           none
+// ==/UserScript==
+
+
+if (document.querySelector(".rlg-error, #cf-error-details") !== null)
+    location.reload();
---
+++ Wikipedia_Donation_Request_Cleaner/2.0.1/Wikipedia_Donation_Request_Cleaner.user.js
@@ -0,0 +1,23 @@
+﻿// ==UserScript==
+// @name            Wikipedia Donation Request Cleaner
+// @namespace       openbytewdrc
+// @author          OpenByte
+// @description     Removes Wikipedia Donation Request.
+// @icon            https://i.imgur.com/mQeok0M.png
+// @require         https://greasyfork.org/scripts/34555-greasemonkey-4-polyfills/code/Greasemonkey%204%20Polyfills.js?version=227108
+// @include         http*://wikipedia.org/*
+// @include         http*://*.wikipedia.org/*
+// @license         MIT License
+// @encoding        utf-8
+// @compatible      firefox
+// @compatible      chrome
+// @compatible      opera
+// @compatible      safari
+// @version         2.0.1
+// @grant           GM_addStyle
+// @grant           GM.addStyle
+// ==/UserScript==
+/* jshint esversion: 6 */
+
+
+GM.addStyle("html body #centralNotice, html body [id*=frbanner], html body [id*=frb-inline] { display: none !important; }");
