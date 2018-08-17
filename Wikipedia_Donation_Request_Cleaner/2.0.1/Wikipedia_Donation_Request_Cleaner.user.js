087ace30beb7e32b0aefddac9176213117a6a726
OpenByte <development.openbyte@gmail.com>
2018-08-17 19:52:02 +0200

    Wikipedia_Donation_Request_Cleaner: Fix meta

--- Wikipedia_Donation_Request_Cleaner/2.0.1/Wikipedia_Donation_Request_Cleaner.user.js
+++ Wikipedia_Donation_Request_Cleaner/2.0.1/Wikipedia_Donation_Request_Cleaner.user.js
@@ -4,20 +4,19 @@
 // @version         2.0.1
 // @author          OpenByte <development.openbyte@gmail.com>
 // @description     Remove wikipedia donation requests.
-// @homepage        https://github.com/OpenByteDev/Userscripts/tree/master/Easy_10_Minute_Mail
+// @homepageURL     https://github.com/OpenByteDev/Userscripts/tree/master/Wikipedia_Donation_Request_Cleaner
 // @icon            https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon.png
 // @icon64          https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon64.png
 // @updateURL       https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/Wikipedia_Donation_Request_Cleaner.meta.js
 // @downloadURL     https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/Wikipedia_Donation_Request_Cleaner.user.js
 // @supportURL      https://github.com/OpenByteDev/Userscripts/issues
 // @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @include         http*://wikipedia.org/*
-// @include         http*://*.wikipedia.org/*
-// @license         MIT License; https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/LICENSE
+// @include         /^https?:\/\/(?:.*?\.)?wikipedia\.org\/.*$/
+// @match           *://*.wikipedia.org/*
+// @run-at          document-start
 // @grant           GM_addStyle
 // @grant           GM.addStyle
 // ==/UserScript==
-/* jshint esversion: 6 */
 
 'use strict';
 
