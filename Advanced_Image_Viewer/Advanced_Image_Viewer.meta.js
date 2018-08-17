f92ea8761340e71b49e12e29196f2f4a3b55db1b
OpenByte <development.openbyte@gmail.com>
2018-08-18 00:20:53 +0200

    Advanced_Image_Viewer: Add README; Update meta; Add current version shortcut

---
+++ Advanced_Image_Viewer/Advanced_Image_Viewer.meta.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name            Advanced Image Viewer
+// @namespace       openbyte/advimgvwr
+// @version         9.5.2
+// @author          OpenByte <development.openbyte@gmail.com>
+// @description     Enhances Image viewing on Open Image in new Tab. Features include: Automatic Scaling, Zoom, Display Resolution, Optimized Rendering and many more...
+// @homepageURL     https://github.com/OpenByteDev/Userscripts/tree/master/Advanced_Image_Viewer
+// @icon            https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Advanced_Image_Viewer/icon32.png
+// @icon64          https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Advanced_Image_Viewer/icon64.png
+// @updateURL       https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Advanced_Image_Viewer/Advanced_Image_Viewer.meta.js
+// @downloadURL     https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Advanced_Image_Viewer/Advanced_Image_Viewer.user.js
+// @supportURL      https://github.com/OpenByteDev/Userscripts/issues
+// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @require         https://github.com/OpenByteDev/Userscripts/raw/master/Userscript_Config_API/1.0.0/Userscript_Config_API.user.js
+// @connect         google.com
+// @connect         api.imgur.com
+// @connect         *
+// @include         *
+// @noframes
+// @license         MIT License; https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/LICENSE
+// @run-at          document-start
+// @grant           GM_addStyle
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @grant           GM_deleteValue
+// @grant           GM_xmlhttpRequest
+// @grant           GM.addStyle
+// @grant           GM.setValue
+// @grant           GM.getValue
+// @grant           GM.deleteValue
+// @grant           GM.xmlHttpRequest
+// ==/UserScript==
---
+++ Advanced_Image_Viewer/README.md
@@ -0,0 +1,5 @@
+# Advanced Image Viewer
+
+Enhances Image viewing on Open Image in new Tab. Features include: Automatic Scaling, Zoom, Display Resolution, Optimized Rendering and many more...
+
+[Download](https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Advanced_Image_Viewer/Advanced_Image_Viewer.user.js)
