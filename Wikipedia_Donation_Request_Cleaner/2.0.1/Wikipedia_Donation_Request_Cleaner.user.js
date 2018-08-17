42b4a1d82981166b6da625004d5f2a5af07ff5b0
OpenByte <development.openbyte@gmail.com>
2018-08-17 19:31:03 +0200

    Wikipedia_Donation_Request_Cleaner: Change meta for compatibility

--- Wikipedia_Donation_Request_Cleaner/2.0.1/Wikipedia_Donation_Request_Cleaner.user.js
+++ Wikipedia_Donation_Request_Cleaner/2.0.1/Wikipedia_Donation_Request_Cleaner.user.js
@@ -4,9 +4,9 @@
 // @version         2.0.1
 // @author          OpenByte <development.openbyte@gmail.com>
 // @description     Remove wikipedia donation requests.
-// @homepageURL     https://github.com/OpenByteDev/Userscripts/tree/master/Easy_10_Minute_Mail
-// @iconURL         https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon.png
-// @icon64URL       https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon64.png
+// @homepage        https://github.com/OpenByteDev/Userscripts/tree/master/Easy_10_Minute_Mail
+// @icon            https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon.png
+// @icon64          https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon64.png
 // @updateURL       https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/Wikipedia_Donation_Request_Cleaner.meta.js
 // @downloadURL     https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/Wikipedia_Donation_Request_Cleaner.user.js
 // @supportURL      https://github.com/OpenByteDev/Userscripts/issues
@@ -20,6 +20,5 @@
 /* jshint esversion: 6 */
 
 'use strict';
-
 
 GM.addStyle('html body #centralNotice, html body [id*=frbanner], html body [id*=frb-inline] { display: none !important; }');
