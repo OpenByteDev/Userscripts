084f5df87bb4d764f4f3aade91860c876c1f3105
OpenByte <development.openbyte@gmail.com>
2018-08-17 23:11:20 +0200

    Always_Show_Resolution_On_GoogleImageSearch: Add README; Update meta; Add current version shortcut

---
+++ Always_Show_Resolution_On_GoogleImageSearch/Always_Show_Resolution_On_GoogleImageSearch.meta.js
@@ -0,0 +1,23 @@
+﻿// ==UserScript==
+// @name            Always Show Resolution On GoogleImageSearch
+// @namespace       openbyte/asragi
+// @version         2.0.3
+// @author          OpenByte <development.openbyte@gmail.com>
+// @description     Always shows image resolution on Google Image Search.
+// @homepageURL     https://github.com/OpenByteDev/Userscripts/tree/master/Always_Show_Resolution_On_GoogleImageSearch
+// @icon            https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Always_Show_Resolution_On_GoogleImageSearch/icon32.png
+// @icon64          https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Always_Show_Resolution_On_GoogleImageSearch/icon64.png
+// @updateURL       https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Always_Show_Resolution_On_GoogleImageSearch/Always_Show_Resolution_On_GoogleImageSearch.meta.js
+// @downloadURL     https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Always_Show_Resolution_On_GoogleImageSearch/Always_Show_Resolution_On_GoogleImageSearch.user.js
+// @supportURL      https://github.com/OpenByteDev/Userscripts/issues
+// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @include         /^https?:\/\/(?:.*?\.)?google.[a-z]+\/.*?[?&]tbm=isch(?:&.*)?$/
+// @license         MIT License; https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/LICENSE
+// @run-at          document-start
+// @grant           GM_setValue
+// @grant           GM_getValue
+// @grant           GM_deleteValue
+// @grant           GM.setValue
+// @grant           GM.getValue
+// @grant           GM.deleteValue
+// ==/UserScript==
---
+++ Always_Show_Resolution_On_GoogleImageSearch/README.md
@@ -0,0 +1,5 @@
+# Always Show Resolution On GoogleImageSearch
+
+Always shows image resolution on Google Image Search.
+
+[Download](https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Always_Show_Resolution_On_GoogleImageSearch/Always_Show_Resolution_On_GoogleImageSearch.user.js)
