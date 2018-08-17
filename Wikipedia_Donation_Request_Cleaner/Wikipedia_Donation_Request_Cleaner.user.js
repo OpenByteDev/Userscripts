bb3c50ab632f6115afe051cc1157fa800ac46a32
OpenByte <development.openbyte@gmail.com>
2018-08-17 19:54:42 +0200

    Wikipedia_Donation_Request_Cleaner: Enable updating

---
+++ Wikipedia_Donation_Request_Cleaner/Wikipedia_Donation_Request_Cleaner.user.js
@@ -0,0 +1,23 @@
+﻿// ==UserScript==
+// @name            Wikipedia Donation Request Cleaner
+// @namespace       openbyte/wdrc
+// @version         2.0.1
+// @author          OpenByte <development.openbyte@gmail.com>
+// @description     Remove wikipedia donation requests.
+// @homepageURL     https://github.com/OpenByteDev/Userscripts/tree/master/Wikipedia_Donation_Request_Cleaner
+// @icon            https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon.png
+// @icon64          https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/icon64.png
+// @updateURL       https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/Wikipedia_Donation_Request_Cleaner.meta.js
+// @downloadURL     https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Wikipedia_Donation_Request_Cleaner/Wikipedia_Donation_Request_Cleaner.user.js
+// @supportURL      https://github.com/OpenByteDev/Userscripts/issues
+// @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @include         /^https?:\/\/(?:.*?\.)?wikipedia\.org\/.*$/
+// @match           *://*.wikipedia.org/*
+// @run-at          document-start
+// @grant           GM_addStyle
+// @grant           GM.addStyle
+// ==/UserScript==
+
+'use strict';
+
+GM.addStyle('html body #centralNotice, html body [id*=frbanner], html body [id*=frb-inline] { display: none !important; }');
