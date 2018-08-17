c3875b2e1f8b3f1a9a12fdbafadb38ad75daa577
OpenByte <development.openbyte@gmail.com>
2018-08-17 22:19:44 +0200

    Easy_10_Minute_Mail: Update meta

--- Easy_10_Minute_Mail/Easy_10_Minute_Mail.meta.js
+++ Easy_10_Minute_Mail/Easy_10_Minute_Mail.meta.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name            Easy 10 Minute Mail
 // @namespace       openbyte/e10mm
-// @author          OpenByte
-// @icon            https://image.ibb.co/bBeDwR/1348cb32_3a6b_4f1c_babf_0aeed1e91e63.png
+// @version         0.1.0
+// @author          OpenByte <development.openbyte@gmail.com>
+// @description     Adds 10MinuteMail button to mail inputs.
 // @homepageURL     https://github.com/OpenByteDev/Userscripts/tree/master/Easy_10_Minute_Mail
+// @icon            https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Easy_10_Minute_Mail/icon32.png
+// @icon64          https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Easy_10_Minute_Mail/icon64.png
 // @updateURL       https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Easy_10_Minute_Mail/Easy_10_Minute_Mail.meta.js
 // @downloadURL     https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/Easy_10_Minute_Mail/Easy_10_Minute_Mail.user.js
 // @supportURL      https://github.com/OpenByteDev/Userscripts/issues
-// @description     Makes using 10 minute Mail more comfortable.
 // @require         https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @include         *
-// @version         0.1.0
+// @license         MIT License; https://raw.githubusercontent.com/OpenByteDev/Userscripts/master/LICENSE
 // @run-at          document-idle
 // @grant           GM_xmlhttpRequest
 // @grant           GM_addStyle
